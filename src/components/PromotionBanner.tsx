b926fe351429872fd73ca574ffb3260ab32c0748
omiq17 <omiq17@gmail.com>
2025-04-28 00:34:16 +0800

    Update promo text css.

--- src/components/PromotionBanner.tsx
+++ src/components/PromotionBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PromotionBanner = () => {
   return (
     <div
-      className="text-white fs-5 fw-bolder text-center py-4 py-md-2 px-20"
+      className="promotion-text text-white fw-bolder text-center py-4 py-md-2 px-20"
       style={{
         background:
           "linear-gradient( 90deg, var(--bs-primary) 0%,var(--bs-secondary) 100%)",
